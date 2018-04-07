a7c2279141c774a73e43b5b4b8606b10e0234ab3
Diego Gonella <diego@meemba.com.ar>
2018-04-07 16:31:52 -0300

    Organizando los Unit Testing

--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -1,18 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class ExampleTest extends TestCase
+class ExampleTest extends FeatureTestCase
 {
-    use DatabaseTransactions;
-    
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
     public function testBasicExample()
     {
         $usuario = factory(\App\User::class)->create([
